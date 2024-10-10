ba881e72cfcf5abab0c5b4ea83cc513fc18c72e7
vishaalS7 <ivishaal1411@gmail.com>
2024-10-10 21:49:37 +0530

    updated ERC20 OG token

---
+++ test/0GlitchTokenTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import "../src/0glitchToken.sol";
+import "../script/DeployOGlitchToken.s.sol";
+
+contract OGlitchTokenTest is Test {
+    OGlitchToken public ogToken;
+    DeployOGlitchToken public deploy;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deploy = new DeployOGlitchToken();
+        ogToken = deploy.run();
+
+        vm.prank(msg.sender);
+        ogToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testbobBalance() public view {
+        assertEq(STARTING_BALANCE, ogToken.balanceOf(bob));
+    }
+
+    function testAllowanceWorks() public {
+        uint initialAllowance = 1000;
+
+        //bob approves alice to spend toke on his behalf 
+        vm.prank(bob);
+        ogToken.approve(alice, initialAllowance);
+
+        uint transferAmount = 500;
+
+        vm.prank(alice);
+        ogToken.transferFrom (bob, alice, transferAmount);
+
+        assertEq (ogToken.balanceOf(alice), transferAmount);
+        assertEq (ogToken.balanceOf(bob),  STARTING_BALANCE - transferAmount);
+
+    }
+}
